e378c63e602aae57e79e32950ab532f6564d88a8
fanz <fanz_lei@126.com>
2017-05-15 09:16:04 +0800

    none

--- src/sys/sys.c
+++ src/sys/sys.c
@@ -11,3 +11,15 @@ void delay(){
 void delays(int d){
 	while(d--);
 }
+
+
+/*
+*使用12MHz晶振（机器周期为1us），延迟
+*/
+void delay_ms(unsigned int ms)
+{
+    unsigned int i = 1000;
+		while(ms--){
+			while(i--);
+		}
+}
